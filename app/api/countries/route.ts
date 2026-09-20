3d048503d0910ab9d09631de9d1625d6cdc5c0d9
thimar <jj1333961@gmail.com>
2026-09-20 11:17:34 +0300

    refactor: remove project configuration and CI files

--- app/api/countries/route.ts
+++
@@ -1,9 +0,0 @@
-import { COUNTRY_RULES } from '@/lib/country-rules'
-
-export const dynamic = 'force-static'
-
-export function GET() {
-  return Response.json({ countries: COUNTRY_RULES }, {
-    headers: { 'Cache-Control': 'public, max-age=86400, stale-while-revalidate=604800', 'X-Content-Type-Options': 'nosniff' },
-  })
-}
